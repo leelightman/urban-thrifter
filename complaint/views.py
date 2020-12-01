9aa237817900dc71bcb0378172bf7dd71488418e
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-12-01 17:56:45 -0500

    copy everything

--- complaint/views.py
+++ complaint/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import render
 from .forms import ComplaintForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from reservation.models import ReservationPost
 
 
-def issue_complaint(request):
+@login_required
+def issue_complaint(request, **kwargs):
+
     if request.method == "POST":
         filled_form = ComplaintForm(request.POST, request.FILES)
         if filled_form.is_valid():
-            note = (
-                "Your complaint about %s has been received. \
-                    We will look into it!!"
-                % (filled_form.cleaned_data["subject"],)
-            )
-            new_form = ComplaintForm
-            filled_form.save()
-            return render(
+            messages.success(
                 request,
-                "complaint/issue_complaint.html",
-                {"complaintform": new_form, "note": note},
+                'Your complaint about "%s" has been received. \
+                        We will look into it.'
+                % (filled_form.cleaned_data["subject"]),
             )
+
+            new_form = ComplaintForm
+            final_form = filled_form.save(commit=False)
+            final_form.issuer = request.user
+            final_form.reservation_post = ReservationPost.objects.get(id=kwargs["pk"])
+
+            try:
+                if request.user.helpseekerprofile:
+                    final_form.receiver = final_form.reservation_post.donor
+            except Exception:
+                if request.user.donorprofile:
+                    final_form.receiver = final_form.reservation_post.helpseeker
+
+            final_form.save()
+            return render(request, "complaint/complaint_form.html", {"form": new_form})
+
     else:
         form = ComplaintForm()
-        return render(
-            request, "complaint/issue_complaint.html", {"complaintform": form}
-        )
+        return render(request, "complaint/complaint_form.html", {"form": form})
